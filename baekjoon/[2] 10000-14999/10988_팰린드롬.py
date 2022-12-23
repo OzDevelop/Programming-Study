19c9b856145db57f023503b0587276d57b4bff78
sangpil byeon <byunsp3610@gmail.com>
2022-12-23 22:41:00 +0900

    no message

---
+++ baekjoon/[2] 10000-14999/10988_팰린드롬.py
@@ -0,0 +1,11 @@
+alp =list(str(input()))
+print(alp)
+
+if list(reversed(alp)) == alp:
+    print(1)
+else:
+    print(0)
+########################
+# a = input()
+# print(1 if a==a[::-1] else 0)
+# print(a[::-1])
